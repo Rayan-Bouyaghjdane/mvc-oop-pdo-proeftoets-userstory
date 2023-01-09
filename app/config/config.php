8989254e193a4c7add414dc927c4466878d5c13c
Bouyaghjdane <332766@student.mboutrecht.nl>
2023-01-09 08:58:12 +0100

    opdracht af

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+// Database constanten
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'be-proeftoets');
+
+
+// Het pad naar de app map
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// De url van de site
+define('URLROOT', 'http://mvc-oop-pdo-proeftoets-userstory.com');
+
+// Naam van de site
+define('SITENAME', 'mvc-oop-pdo-proeftoets-userstory');
